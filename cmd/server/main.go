41644c9d27b029dc811d2fba3f6ba5863f681249
rajeshbond <rajesh.sntsolutions@gmail.com>
2026-06-13 22:46:48 +0530

    new file:   .env 	modified:   cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/rajbond/data2/internal/handlers"
 )
@@ -19,14 +20,48 @@ func main() {
 		handlers.GetLatestCycle,
 	)
 
-	log.Println(
-		"Server Started On :8080",
-	)
+	// Render provides PORT dynamically
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback for local testing
+	}
+
+	log.Println("Server Started On :" + port)
 
 	log.Fatal(
-		http.ListenAndServe(
-			":8080",
-			nil,
-		),
+		http.ListenAndServe(":"+port, nil),
 	)
 }
+
+// package main
+
+// import (
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/rajbond/data2/internal/handlers"
+// )
+
+// func main() {
+
+// 	http.HandleFunc(
+// 		"/api/v1/cycle",
+// 		handlers.CreateCycle,
+// 	)
+
+// 	http.HandleFunc(
+// 		"/api/v1/cycle/latest",
+// 		handlers.GetLatestCycle,
+// 	)
+
+// 	log.Println(
+// 		"Server Started On :8080",
+// 	)
+
+// 	log.Fatal(
+// 		http.ListenAndServe(
+// 			":8080",
+// 			nil,
+// 		),
+// 	)
+// }
