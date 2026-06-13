b602fb8754db8f872a87ffe94f1fae8ed1fc5caf
rajeshbond <rajesh.sntsolutions@gmail.com>
2026-06-13 23:35:15 +0530

    modified:   internal/handlers/cycle.post.go 	modified:   internal/handlers/cycle_get.go 	modified:   internal/storage/cycle.go

--- internal/handlers/cycle_get.go
+++ internal/handlers/cycle_get.go
@@ -24,7 +24,23 @@ func GetLatestCycle(
 	}
 
 	storage.Mu.RLock()
-	data := storage.LatestCycle
+
+	response := map[string]any{
+		"device_id": storage.LatestCycle.DeviceID,
+
+		"cycle_time_sec": storage.LatestCycle.CycleTimeSec,
+
+		"production_count": storage.LatestCycle.ProductionCount,
+
+		"server_time": storage.LatestServerTime,
+
+		"gap_ms": storage.LatestGapMs,
+
+		"gap_sec": float64(storage.LatestGapMs) / 1000,
+
+		"handler_duration_us": storage.LatestHandlerDurationUs,
+	}
+
 	storage.Mu.RUnlock()
 
 	w.Header().Set(
@@ -32,5 +48,42 @@ func GetLatestCycle(
 		"application/json",
 	)
 
-	json.NewEncoder(w).Encode(data)
+	json.NewEncoder(w).Encode(response)
 }
+
+// package handlers
+
+// import (
+// 	"encoding/json"
+// 	"net/http"
+
+// 	"github.com/rajbond/data2/internal/storage"
+// )
+
+// func GetLatestCycle(
+// 	w http.ResponseWriter,
+// 	r *http.Request,
+// ) {
+
+// 	if r.Method != http.MethodGet {
+
+// 		http.Error(
+// 			w,
+// 			"GET only",
+// 			http.StatusMethodNotAllowed,
+// 		)
+
+// 		return
+// 	}
+
+// 	storage.Mu.RLock()
+// 	data := storage.LatestCycle
+// 	storage.Mu.RUnlock()
+
+// 	w.Header().Set(
+// 		"Content-Type",
+// 		"application/json",
+// 	)
+
+// 	json.NewEncoder(w).Encode(data)
+// }
--- internal/storage/cycle.go
+++ internal/storage/cycle.go
@@ -12,10 +12,35 @@ var (
 
 	LatestCycle models.CycleRequest
 
+	LatestGapMs int64
+
+	LatestServerTime string
+
+	LatestHandlerDurationUs int64
+
 	LastRequestTime = make(
 		map[string]time.Time,
 	)
 )
+
+// package storage
+
+// import (
+// 	"sync"
+// 	"time"
+
+// 	"github.com/rajbond/data2/internal/models"
+// )
+
+// var (
+// 	Mu sync.RWMutex
+
+// 	LatestCycle models.CycleRequest
+
+// 	LastRequestTime = make(
+// 		map[string]time.Time,
+// 	)
+// )
 
 // package storage
 
